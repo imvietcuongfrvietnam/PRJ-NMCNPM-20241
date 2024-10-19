ec22dd13a0ca4890cf8d5592d062d814c30c9a05
Ngân Văn Thiện <121108951+NganThien@users.noreply.github.com>
2024-10-19 09:22:12 +0700

    .

--- HelloWorld.java
+++ HelloWorld.java
@@ -9,8 +9,8 @@ public class HelloWorld {
         System.out.println(array_out); // array_out=null;
         int array_in[] = new int[5];
         System.out.println(array_in); // loi bien dich
-        A A_out = new A();
-        System.out.println(A_out); // A_out=null;
+        HelloWorld.A_out = new A();
+        System.out.println(HelloWorld.A_out); // A_out=null;
         A A_in = new A();
         System.out.println(A_in);
         //System.out.println(A_in); // loi bien dich
