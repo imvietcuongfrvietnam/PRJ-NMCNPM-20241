2dfddc817f7bd059815d3b4dbbd4d5fed03fb45c
vietcuong <vietcuong2k182@gmail.com>
2024-10-21 21:10:10 +0700

    object & Switcher

---
+++ src/main/java/model/Switcher.java
@@ -0,0 +1,21 @@
+package model;
+
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Switcher {
+    private static Stage stage;
+
+    private static Scene scene;
+
+    private static Parent root;
+
+    public void goSignUp(){
+
+    }
+    public void goHomePage(){
+
+    }
+
+}
