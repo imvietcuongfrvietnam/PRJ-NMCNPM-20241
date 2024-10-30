4861554a87f0f98b48ed7494c99b5f6238fc5530
vietcuong <vietcuong2k182@gmail.com>
2024-10-30 22:27:20 +0700

    add LogInController

--- src/main/java/myapp/controller/LogInController.java
+++ src/main/java/myapp/controller/LogInController.java
@@ -6,7 +6,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import myapp.model.manager.Switcher;
-public class SignInController {
+public class LogInController {
 
     @FXML
     private Button signInButton;
