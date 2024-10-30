858b4be41ce02798efd7541bd03fdde07fce0261
Ngân Văn Thiện <121108951+NganThien@users.noreply.github.com>
2024-10-30 13:48:27 +0700

    .

--- src/main/java/myapp/Main.java
+++ src/main/java/myapp/Main.java
@@ -7,7 +7,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import model.Switcher;
+import myapp.controller.Switcher;
 
 public class Main extends Application {
 
