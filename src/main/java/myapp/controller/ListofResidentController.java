bb6bb46d4084365fcb066ce3021cf3bfba08567c
vietcuong <vietcuong2k182@gmail.com>
2024-11-07 15:27:28 +0700

    pulled hieu

--- src/main/java/myapp/controller/ListofResidentController.java
+++ src/main/java/myapp/controller/ListofResidentController.java
@@ -20,7 +20,7 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ResourceBundle;
 
-public class ListofResidentController implements Initializable {
+public class ListofResidentController extends BaseController implements Initializable {
     @FXML
     private StackPane mainStackPane;
     @FXML
--- src/main/java/myapp/controller/LogInController.java
+++ src/main/java/myapp/controller/LogInController.java
@@ -119,4 +119,5 @@ public class LogInController extends BaseController{
         }
 
     }
+
 }
